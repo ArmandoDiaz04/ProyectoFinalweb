edbab91a6c8c89b28a24b4cb6ccec4476585cca4
ArmandoDiaz04 <diazarmando783@gmail.com>
2023-05-22 21:57:24 -0600

    subiendo proyecto

---
+++ proyectoFinal/Controllers/InicioController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace proyectoFinal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InicioController : Controller {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ proyectoFinal/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace proyectoFinal.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
